154c20e89702c8ca55ca46cc43977cfc219d5fbc
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-02 16:46:10 +0530

    initialized app and installed tailwindCss

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import  ReactDOM from "react-dom/client";
+
+
+
+const App=()=>{
+
+    return <h1>Hello wrldl</h1>
+}
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
+
+export default App;
+
+
