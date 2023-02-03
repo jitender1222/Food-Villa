4446d1f13660f907a2fba82c131d183bfe23e088
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-03 14:38:17 +0530

    routing added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,52 @@
 import React from "react";
 
 import  ReactDOM from "react-dom/client";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import {
+    createBrowserRouter,
+    Outlet,
+    RouterProvider,
+  } from "react-router-dom";
+import ErrorPage from "./Components/ErrorPage";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
 
+ const App=()=>{
+    return <>
+    <Header />
+    <Outlet />
 
+    </>
+ }
 
-const App=()=>{
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<App />,
+        errorElement:<ErrorPage />,
 
-    return <h1>Hello wrldl</h1>
-}
-
+        children:[
+            {
+                path:"/",
+                element:<Body />
+            },
+            {
+                path:"/about",
+                element:<About/>
+            },
+            {
+                path:"/contact",
+                element:<Contact />
+            }
+        ]
+    },
+])  
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
 
 export default App;
 
