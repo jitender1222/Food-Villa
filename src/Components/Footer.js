bf822e8997695042990a19a0033fc10b1eac0c5a
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-06 23:49:47 +0530

    added Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Footer=()=>{
+
+    return (
+        <>
+        <div class="bg-green-700 w-[100%] p-6 text-center bottom-0">Footer</div>
+        </>
+    )
+}
+
+export default Footer
